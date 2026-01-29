96f0e44684acc8f0490fc54d57fae79ec1ce7ef0
Kyle Methot <kwmethot88@sccsc.edu>
2026-01-29 12:27:08 -0500

    Kyle Methot - A-Stubbs-Lab-2 Code Review

---
+++ lab-2-databases-asherstubbs/A-Stubbs-Lab-2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_Stubbs_Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cityBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.cityBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.cityDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'cityDBDataSet.City' table. You can move, or remove it, as needed.
+            this.cityTableAdapter.Fill(this.cityDBDataSet.City);
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if(cbSort.SelectedItem != null)
+            {
+                if(cbSort.SelectedItem.ToString() == "Population ASC")
+                {
+                    DataTable report = this.cityTableAdapter.PopASCTable();
+                    Report window = new Report();
+                    window.Load(report);
+                    window.ShowDialog();
+                }
+                if(cbSort.SelectedItem.ToString() == "Population DESC")
+                {
+                    DataTable report = this.cityTableAdapter.PopDESCTable();
+                    Report window = new Report();
+                    window.Load(report);
+                    window.ShowDialog();
+                }
+                if(cbSort.SelectedItem.ToString() == "City Alphabetical")
+                {
+                    DataTable report = this.cityTableAdapter.AlphaTable();
+                    Report window = new Report();
+                    window.Load(report);
+                    window.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Must select a sort type!");
+                return;
+            }
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if(cbPop.SelectedItem != null )
+            {
+                if (cbPop.SelectedItem.ToString() == "Total")
+                {
+                    MessageBox.Show("Total population in all cities is: " + this.cityTableAdapter.TotalPop());
+                }
+                if(cbPop.SelectedItem.ToString() == "Average")
+                {
+                    MessageBox.Show("Average population for all cities is: "+this.cityTableAdapter.AvgPop());
+                }
+                if(cbPop.SelectedItem.ToString() == "Highest")
+                {
+                    DataTable report = this.cityTableAdapter.MaxTable();
+                    Report window = new Report();
+                    window.Load(report);
+                    window.ShowDialog();
+                }
+                if(cbPop.SelectedItem.ToString() == "Lowest")
+                {
+                    DataTable report = this.cityTableAdapter.MinTable();
+                    Report window = new Report();
+                    window.Load(report);
+                    window.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Must select population type!");
+                return;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            cbPop.SelectedItem = null;
+            cbSort.SelectedItem = null;
+        }
+    }
+}
---
+++ lab-2-databases-asherstubbs/A-Stubbs-Lab-2/Report.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace A_Stubbs_Lab_2
+{
+    partial class Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReport = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReport
+            // 
+            this.dgvReport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReport.Location = new System.Drawing.Point(-1, 1);
+            this.dgvReport.Name = "dgvReport";
+            this.dgvReport.ReadOnly = true;
+            this.dgvReport.Size = new System.Drawing.Size(548, 437);
+            this.dgvReport.TabIndex = 0;
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 438);
+            this.Controls.Add(this.dgvReport);
+            this.Name = "Report";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Report";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReport;
+    }
+}
